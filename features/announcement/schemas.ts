2317e59c10864224d13b14e18aa36d17214fe3ef
Lautaro <lautaromateol@gmail.com>
2026-01-30 12:35:44 -0300

    update: title prop added to announcement schema

--- features/announcement/schemas.ts
+++ features/announcement/schemas.ts
@@ -1,5 +1,6 @@
 import z from "zod";
 
 export const createAnnouncementSchema = z.object({
+    title: z.string().min(1, { message: "Ingresa el titulo del anuncio." }),
     message: z.string().min(1, { message: "No puedes enviar un anuncio vacío." }).max(300, { message: "Ingresa un maximo de 300 caracteres." }),
 })
