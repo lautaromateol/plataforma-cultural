8f60ec106c11679ac08898be5a665cb64b682141
Lautaro <lautaromateol@gmail.com>
2026-01-30 19:13:02 -0300

    refactor: actualizar hook de apertura de aviso con subjectId
    - Agregar estado de subjectId al hook
    - Actualizar método open() para recibir subjectId como parámetro
    - Resetear subjectId al cerrar el modal

--- features/announcement/hooks/use-open-create-announcement.ts
+++ features/announcement/hooks/use-open-create-announcement.ts
@@ -1,14 +1,16 @@
 import { create } from "zustand";
 
 interface UseOpenCreateAnnouncementProps {
+    subjectId: string;
     isOpen: boolean;
-    open: () => void;
+    open: (subjectId: string) => void;
     close: () => void;
 }
 
 export const useOpenCreateAnnouncement = create<UseOpenCreateAnnouncementProps>((set) => ({
+    subjectId: "",
     isOpen: false,
-    open: () => set({ isOpen: true }),
-    close: () => set({ isOpen: false })
+    open: (subjectId: string) => set({ isOpen: true, subjectId }),
+    close: () => set({ isOpen: false, subjectId: "" })
 }))
 
