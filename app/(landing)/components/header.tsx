5f5f6562514b43ffab632f1f534da2f6476d39c7
Lautaro <lautaromateol@gmail.com>
2026-02-05 18:07:16 -0300

    add: school logo

--- app/(landing)/components/header.tsx
+++ app/(landing)/components/header.tsx
@@ -1,25 +1,31 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { GraduationCap } from "lucide-react";
 import Link from "next/link";
 import { EnrollmentButton } from "@/features/enrollment";
+import Image from "next/image";
 
 export function Header() {
     return (
         <header className="sticky top-0 bg-white w-full flex items-center justify-around py-4 border-b border-gray-100 z-10">
-            <div className="flex items-center gap-x-2">
-                <GraduationCap className="h-8 w-8 text-indigo-600" />
-                <h1 className="text-2xl font-bold">Centro Cultural Correntino</h1>
+            <div className="flex items-center gap-x-4">
+                <div className="w-40 h-20 relative">
+                    <Image
+                        src="/logo.png"
+                        alt="Logo"
+                        fill
+                        className="object-contain"
+                    />
+                </div>
+                <nav>
+                    <ul className="flex items-center text-sm gap-6">
+                        <li><a href="#inicio" className="hover:text-indigo-500">Inicio</a></li>
+                        <li><a href="#modalidades" className="hover:text-indigo-500">Modalidades</a></li>
+                        <li><a href="#preguntas" className="hover:text-indigo-500">Preguntas</a></li>
+                        <li><a href="#ubicacion" className="hover:text-indigo-500">Ubicación</a></li>
+                    </ul>
+                </nav>
             </div>
-            <nav>
-                <ul className="flex items-center text-sm gap-6">
-                    <li><a href="#inicio" className="hover:text-indigo-500">Inicio</a></li>
-                    <li><a href="#modalidades" className="hover:text-indigo-500">Modalidades</a></li>
-                    <li><a href="#preguntas" className="hover:text-indigo-500">Preguntas</a></li>
-                    <li><a href="#ubicacion" className="hover:text-indigo-500">Ubicación</a></li>
-                </ul>
-            </nav>
             <nav className="flex items-center justify-center gap-x-6">
                 <Button asChild variant="ghost">
                     <Link href="/login" className="text-sm hover:text-indigo-500">
